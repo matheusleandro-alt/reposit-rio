98a6bac42458cf8087eabb3af33dd7b1a8c34f6c
matheusleandro-alt <matheus.leandro@unifesp.br>
2026-04-06 23:32:18 -0300

    Add files via upload

---
+++ 9 (1).c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	float n1;
+	printf("Digite o lado do quadrado em cm: ");
+	scanf("%f", &n1);
+	printf("Dobro da área: %f", n1*n1*2);
+}
---
+++ oito.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    float n1;
+    printf("Digite a distancia em m: ");
+    scanf("%f", &n1);
+    printf("Distancia em cm: %f", n1*100);
+}
